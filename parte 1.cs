c15ebc23e4f4c0637316e9a77bf04db4265f90b3
italo0072 <87822003+italo0072@users.noreply.github.com>
2021-08-02 22:18:15 -0600

    Update and rename xd.cs to parte 1.cs

---
+++ parte 1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fghj
+{
+    class Program
+   {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string saludo = "hola";
+            string nombre = " italo Antonio";
+
+            Console.WriteLine(saludo + nombre);
+          
+
+            Console.ReadKey();
+
+
+
+        }
+    }    
+}
