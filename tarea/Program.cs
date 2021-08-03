39abcd3650e2f67a6261122ff15de5bcdee41b86
italo Garcia <lobo123445@outlook.es>
2021-08-02 21:22:12 -0600

    Agregar archivos de proyecto.

---
+++ tarea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string saludo = "hola";
+            string nombre = " italo Antonio";
+
+            Console.WriteLine(saludo + nombre);
+          
+
+            Console.ReadKey();
+
+
+
+        }
+    }    
+}
